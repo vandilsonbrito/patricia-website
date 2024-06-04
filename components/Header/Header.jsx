4fd418bc6867dd864132ba608f86fab724fd5257
vandilsonbrito <vandilson25@outlook.com>
2024-06-04 16:26:13 -0300

    Create header component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full h-[50px] bg-secundary px-3 sm:px-8 lg:pt-8 font-medium z-[100] relative">
+      <nav className="w-full h-full flex justify-center items-center">
+        <ul className="w-[90%] md:w-[80%] xl:w-[70%] items-end justify-around text-sm md:text-xl lg:text-[1.3rem] text-black flex">
+            <li /* className="hover:text-[#a44fd4]" */>
+                <Link href='/'>Home</Link>
+            </li>
+            <li /* className="hover:text-[#a44fd4]" */>
+                <Link href='/sobre'>Sobre</Link>
+            </li>
+            <li /* className="hover:text-[#a44fd4]" */>
+                <Link href='/youtube'>Youtube</Link>
+            </li>
+            <li /* className="hover:text-[#a44fd4]" */>
+                <Link href='/contato'>Contato</Link>
+            </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
