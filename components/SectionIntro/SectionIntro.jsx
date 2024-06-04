a3da41a246c11a07c9baee9a24643bb5ca0e3684
vandilsonbrito <vandilson25@outlook.com>
2024-06-04 16:26:30 -0300

    Create sectionIntro component

---
+++ components/SectionIntro/SectionIntro.jsx
@@ -0,0 +1,23 @@
+
+import Image from 'next/image';
+import BlankNote from '@/public/img-design/blank-page.jpg';
+
+
+export default function SectionIntro() {
+
+  return (
+      <section className='w-full min-h-[400px] h-fit flex flex-col items-center px-8 py-5 text-left bg-primary font-medium text-lg sm:text-2xl md:text-3xl xl:text-[2rem] text-[#000] pb-10 lg:py-12 lg:pb-5 overflow-hidden'>
+      <div className="w-full h-full flex flex-col justify-center items-center  ">
+            <h2 className="sm:w-[70%] lg:w-[62%] min-h-[100px] my-3 sm:my-5 leading-snug lg:mt-5 lg:mb-10 text-center md:text-left">Você precisa escrever seu <span className='text-[#b35ebd] font-semibold'>TCC, Artigo, Projeto de Pesquisa, Relatório</span> e não sabe como ou por onde começar?</h2>
+            <Image 
+              className="w-[80%] sm:w-[70%] lg:w-[50%] rounded-md mt-3 lg:-mt-2 sm:mt-0" 
+              src={BlankNote} 
+              alt="Página de caderno em branco" 
+                
+              />
+            <p className="mt-3 sm:mt-5 lg:my-6 sm:text-lg md:text-2xl lg:text-3xl font-bold py-3 px-4 rounded-md ">Você está no lugar certo!</p>
+          </div>
+      </section>
+
+  )
+}
