32f4ebb0b3e00a9c639a509ab1a6062832122bc8
vandilsonbrito <vandilson25@outlook.com>
2024-09-06 12:15:56 -0300

    Chore: delete bg-color and change hero img measurements for mobile

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const josefin = Josefin_Slab({ subsets: ["latin"], weight:['200', '300', '400', 
 export default function Banner() {
 
   return (
-    <section className="w-full h-fit min-h-[400px] xl:min-h-screen bg-secundary flex flex-col xl:flex-row px-5 md:px-10 xl:pt-14 xl:pb-5" /* style={{ minHeight: window.innerWidth >= 1280 ? `calc(100vh - 50px)` : 'auto' }} */
+    <section className="w-full h-fit min-h-[400px] xl:min-h-screen flex flex-col xl:flex-row px-5 md:px-10 xl:pt-14 xl:pb-5" /* style={{ minHeight: window.innerWidth >= 1280 ? `calc(100vh - 50px)` : 'auto' }} */
       >
         <div className="w-full h-full text-black flex flex-col xl:flex-row justify-evenly items-center" >
             <div className="w-full h-fit pt-10 py-5 md:px-10 md:py-10 lg:py-16 xl:w-[700px] xl:h-full flex justify-center items-center md:mt-10 lg:left-20 lg:px-20 xl:px-0" >
@@ -20,7 +20,7 @@ export default function Banner() {
                 </h1>
             </div>
             <div className="w-full h-1/2 xl:w-fit xl:h-full xl:pt-10 flex flex-col items-center justify-end overflow-hidden">
-                  <div className="profile-picture w-[335px] h-[410px]  lg:w-[405px] lg:h-[490px]" role="img" aria-label="Imagem Professora Patrícia Cruz">
+                  <div className="profile-picture w-[290px] h-[365px]  lg:w-[405px] lg:h-[490px]" role="img" aria-label="Imagem Professora Patrícia Cruz">
                     <div className='profile-overlay'></div> 
                   </div>
             </div>
